75ba98cd6f9ea4a41b924a7acde1325733393480
Alon A. Rabinowitz <alonrab@outlook.com>
2024-11-04 19:22:34 +0200

    first sample

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+
+// put function declarations here:
+
+void setup()
+{
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.printf("Hello World! %d\n", 182);
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+}
+
+// put function definitions here:
